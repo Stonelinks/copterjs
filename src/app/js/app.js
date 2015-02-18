64be1d395fb243e977711440bcd84a7245dffdb2
Adam Sax <asax@airware.com>
2015-02-18 14:37:58 -0800

    converted logs to bootstrap view

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,2 +1,40 @@
+var $			= require('jquery');
+var Backbone	= require('backbone');
+var io			= require('socket.io-client');
+Backbone.$ = $;
+var Marionette 	= require('backbone.Marionette');
 
 
+var app = new Marionette.Application();
+window.app = app;
+
+app.addRegions({
+	content: '#content'
+});
+
+var Router = Marionette.AppRouter.extend({
+
+	routes: {
+		'console' : 'showConsole'
+	},
+
+	showConsole: function() {
+		var ConsoleView = require('./view/console');
+		app.getRegion('content').show(new ConsoleView());
+	}
+
+});
+
+// open the socket
+app.addInitializer(function(opts) {
+	console.log("opening socket");
+	this.socket = io('http://localhost:4545');
+}.bind(app));
+
+// start the router
+app.addInitializer(function(opts) {
+	this.router = new Router();
+	Backbone.history.start({pushState: true});
+});
+
+app.start();
--- src/server/services/copter.js
+++ src/server/services/copter.js
@@ -1,9 +1,18 @@
+var io = require('socket.io-client');
+
 function CopterService() {
 
 };
 
 CopterService.prototype.start = function() {
 	console.log("copter server in start");
+
+	this.socket = io.connect('http://localhost:4545');
+
+	setInterval(function() {
+		this.socket.emit('log', 'some message');
+		console.log('logging');
+	}.bind(this), 500);
 };
 
 
